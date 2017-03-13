01db0dc69498ab93a1a00fdeef77d0c6e9cf9158
Leloz Suliman <leloz997@gmail.com>
2017-03-13 23:16:11 +0300

    Done.

---
+++ spaceship.js
@@ -0,0 +1,33 @@
+
+class Spaceship {
+	constructor(name, crew, phasers, shields) {
+		this.name = name;
+		this.crew = crew;
+		this.phasers = phasers;
+		this.shields = shields;
+		this.cloaked = false;
+		this.warpDrive = 'disengaged';
+		if (crew.length === 0) {
+			this.docked = true;
+		}
+
+		else{
+			this.docked = false;
+		}
+
+		this.phasersCharge = 'uncharged';
+		this.informCrew();
+	}
+
+	informCrew(){
+		this.crew.forEach(crewMember => {
+			crewMember.currentShip = this;
+		});
+
+	}
+}
+
+
+
+
+
